72c1440152b7e14c1be1a61cb0aaa5a742a357a4
JediMasterAle <alexandre.carvalhal@gmail.com>
2021-11-30 16:01:59 -0300

    Requisição API

---
+++ src/connection/api.js
@@ -0,0 +1,9 @@
+export default {
+    url: "http://localhost:8000/api/produto",
+    headers: {
+        headers: {
+            "Content-Type": "multipart/form-data",
+            Accept: "application/json",
+        },
+    }
+}
